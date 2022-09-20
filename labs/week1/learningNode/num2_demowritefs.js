6f8ea2403efe5ce5e9403c2077ce897c1365e9e1
ruizalberto <albertoruizb14@gmail.com>
2022-09-20 13:33:53 +0100

    Fixed directory paths

--- labs/week1/learningNode/num2_demowritefs.js
+++ labs/week1/learningNode/num2_demowritefs.js
@@ -6,7 +6,7 @@ var fs = require('fs')
 
 const content = 'Some content heree!' //data to be written
 
-fs.writeFile('./dataWrite.txt', content, err => { //writes data to file (bydefault it replaces if the files exists). 4 parameters file, data, options, and a callback function that is called when the method is executed
+fs.writeFile('learningNode/dataWrite.txt', content, err => { //writes data to file (bydefault it replaces if the files exists). 4 parameters file, data, options, and a callback function that is called when the method is executed
   if (err) {
     console.error(err+'There was a write error') // error is there's some issues
     return
