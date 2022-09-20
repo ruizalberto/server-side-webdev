64ea772ad0fb72317eccfa2e68bfc0d748449be1
ruizalberto <albertoruizb14@gmail.com>
2022-09-20 13:43:26 +0100

    Fixed num5

--- labs/week1/learningNode/num5_demo1export.js
+++ labs/week1/learningNode/num5_demo1export.js
@@ -1,2 +1,2 @@
-exports.myText='Kashif' //used to export values from a JavaScript module. Exported values can then be imported into other programs 
-exports.myAddress='MTU bishopstown'
+exports.myText='Alberto' //used to export values from a JavaScript module. Exported values can then be imported into other programs 
+exports.myAddress='MTU Bishopstown'
