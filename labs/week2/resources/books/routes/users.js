654afd9d1ae3d952657e6e33584ddc76e3e2588d
ruizalberto <albertoruizb14@gmail.com>
2022-09-27 13:45:26 +0100

    Added resources that doesn't work

--- labs/week2/resources/books/routes/users.js
+++ labs/week2/resources/books/routes/users.js
@@ -2,11 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) { // /user
+router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-});
-router.get('/create', function(req, res, next) {
-  res.send('respond with a resource/create');
 });
 
 module.exports = router;
