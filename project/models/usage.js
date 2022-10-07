54f9dc0d35866e73726d30d28278f2455b5ac9a6
ruizalberto <albertoruizb14@gmail.com>
2022-10-07 09:34:42 +0100

    Added models

---
+++ project/models/usage.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+var usageSchemaful = new Schema({
+    name: {
+        type: String,
+        required: true,
+    }
+});
