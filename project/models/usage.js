ddd8d8c66811874a65be02c90cb714ff32ec4366
ruizalberto <albertoruizb14@gmail.com>
2022-10-07 11:14:35 +0100

    Removed mongoose-currency dependency

--- project/models/usage.js
+++ project/models/usage.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-require('mongoose-currency').loadType(mongoose);
-var Currency = mongoose.Types.Currency;
+//require('mongoose-currency').loadType(mongoose);
+//var Currency = mongoose.Types.Currency;
 
 var usageSchemaful = new Schema({
     name: {
