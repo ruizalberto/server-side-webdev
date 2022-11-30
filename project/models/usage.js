77a7133fa99131fbd8728ffaefd1e5e9b651e2a4
ruizalberto <albertoruizb14@gmail.com>
2022-11-30 11:27:18 +0000

    Project phase 2 delivery

--- project/models/usage.js
+++ project/models/usage.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//require('mongoose-currency').loadType(mongoose);
-//var Currency = mongoose.Types.Currency;
-
 var usageSchemaful = new Schema({
     name: {
         type: String,
